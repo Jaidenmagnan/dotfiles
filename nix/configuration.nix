b2ff155d5425b6bf97d185149ea0eebb45d6f4e0
Jaiden Magnan <jaidenmagnan@gmail.com>
2024-03-15 12:20:21 -0400

    homemanager

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -2,12 +2,13 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs,inputs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
     ];
 
   # Bootloader.
@@ -57,6 +58,13 @@
     description = "Jaiden Magnan";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [];
+  };
+
+  home-manager = {
+	extraSpecialArgs = {inherit inputs; };
+	users = {
+		"jaiden" = import ./home.nix;
+	};
   };
 
   # Allow unfree packages
