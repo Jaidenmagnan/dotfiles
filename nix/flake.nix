e2ba98d82bc9e06814fc0271919b65051c5107c2
Jaiden Magnan <jaidenmagnan@gmail.com>
2024-03-15 12:14:55 -0400

    new flake

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,20 +1,22 @@
 {
+  description = "Nixos config flake";
 
-    description = "My configuration flake";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
-    inputs = {
-	    nixpkgs.url = "nixpkgs/nixos-23.11";
-        };
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
 
-    outputs = {self, nixpkgs, ...}:
-    let
-	lib = nixpkgs.lib;
-    in {
-	nixosConfigurations = {
-	    nixos = lib.nixosSystem {
-		system = "x86_64-linux";
-		modules = [ ./configuration.nix ];
-	    };
-	};
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        # inputs.home-manager.nixosModules.default
+      ];
     };
+  };
 }
